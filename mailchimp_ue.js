c4f306f85618d6c91708d93ad9f2fc758fadac08
Esau Grover Ocrospoma Callupe <eocrospoma@utec.local>
2018-03-09 11:04:09 -0500

    base64

--- mailchimp_ue.js
+++ mailchimp_ue.js
@@ -4,9 +4,10 @@
  * @NModuleScope Public
  */
 
-define(['N/https', 'N/log'], 
-	function(https,log) {
+define(['N/https', 'N/log','N/encode'], 
+	function(https,log,encode) {
 		
+	
 	function Test_AfterSubmit(context) {
 
 
@@ -15,6 +16,9 @@ define(['N/https', 'N/log'],
 		var listid="LIST_ID";
 		var URL = 'https://us14.api.mailchimp.com/3.0/lists/'+listid+'/members/';
   		
+  		var apikeybase64= toBase64("anystring:"+apiKey);
+  		log.error("apikeybase64",apikeybase64);
+
   		var jsonVar = [{
 			"email_address": email_new,
 			"status":"subscribed",
@@ -25,12 +29,12 @@ define(['N/https', 'N/log'],
 
 
         var headers = [{
-      		 "Authorization":"Basic "+apiKey,
+      		 "Authorization":"Basic <"+apikeybase64+">",
        		 "Content-Type" :"application/json"
         }];
-		var header= JSON.stringify(headers);
+		//var header= JSON.stringify(headers);
 		log.error("Data",data);
-        log.error("Header",header);
+        log.error("Header",headers);
 		log.error("URL",URL);
 
 		try{
@@ -38,19 +42,27 @@ define(['N/https', 'N/log'],
                 https.post({
                     url: URL,
                     body: data,
-                    headers: header
+                    headers: headers
                 });
         log.error("Pass");
 
 		}catch(e){
-			log.error(e);
+			log.error("ERROR",e);
 		}
 
        
         //var response = nlapiRequestURL(URL, null, header,'GET');
-		log.error(response);
+		log.error("RESPONSE",response);
 		 return true;
        
+	}
+
+	function toBase64(stringInput){
+	    return encode.convert({
+	        string: stringInput,
+	        inputEncoding: encode.Encoding.UTF_8,
+	        outputEncoding: encode.Encoding.BASE_64
+	    });
 	}
 	
 
